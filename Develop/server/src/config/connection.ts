cae49e5b7c8ba4a95092ad62fbb1e4f117a934f4
TivonaDe <153453135+TivonaDe@users.noreply.github.com>
2025-01-25 17:08:17 -0500

    Update connection.ts

--- Develop/server/src/config/connection.ts
+++ Develop/server/src/config/connection.ts
@@ -3,6 +3,6 @@ dotenv.config();
 
 import mongoose from 'mongoose';
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/book-search');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://devonacanada1:Password@cluster0.ye3ew.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0);
 
 export default mongoose.connection
